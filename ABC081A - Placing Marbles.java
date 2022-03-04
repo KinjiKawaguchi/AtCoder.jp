bcdb9e12cfb681bda54ec901637d2c2607c02243
河口　欣仁 <93261102+KinjiKawaguchi@users.noreply.github.com>
2022-03-04 11:42:02 +0800

    Create ABC081A - Placing Marbles.java

---
+++ ABC081A - Placing Marbles.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+ 
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int count=0;
+        String a = sc.next();
+        String[] ary = a.split("");
+        for(int i = 0; i<ary.length;i++){
+            if("1".equals(ary[i])){
+                count++;
+            }
+        }
+        System.out.println(count);
+    }

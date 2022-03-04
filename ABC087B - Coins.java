c2d5eb9b7193ab584783e6cc0092d08d4d4cff6e
河口　欣仁 <93261102+KinjiKawaguchi@users.noreply.github.com>
2022-03-04 11:43:11 +0800

    Update ABC087B - Coins.java

--- ABC087B - Coins.java
+++ ABC087B - Coins.java
@@ -3,9 +3,16 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        if((a*b)%2==0)System.out.print("Even");
-        else System.out.print("Odd");
+        int A=sc.nextInt(),B=sc.nextInt(),C=sc.nextInt(),X=sc.nextInt(),count=0;
+        for(int a = 0;a<=A;a++){
+            for(int b = 0;b<=B;b++){
+                for(int c = 0;c<=C;c++){
+                    if(a*500 + b*100 + c*50 == X){
+                        count++;
+                    }
+                }
+            }
+        }
+        System.out.print(count);
     }
 }
